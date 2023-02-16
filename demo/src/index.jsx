1df76d053bfd034870c65bda4befa9b3b9fa1629
minghl <minghao.li2@uzh.ch>
2023-02-16 18:33:47 +0800

    2.16学习

--- demo/src/index.jsx
+++ demo/src/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@/index.less'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <div>小李加油</div>
-);
 
+let text = {10:10}
+root.render(
+    <div>
+        {text}
+    </div>
+);
